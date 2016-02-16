eb2723d4687e5efa41595d04f30aae2f450f9db2
David <yoxisem544@gmail.com>
2016-02-16 19:48:18 +0800

    initial commit

--- Dispatch_Work/ViewController.swift
+++ Dispatch_Work/ViewController.swift
@@ -14,6 +14,70 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
+	
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+		start()
+	}
+	
+	func start() {
+		var queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+		var group = dispatch_group_create()
+		
+		func work1() {
+			print("work1 started")
+			NSThread.sleepForTimeInterval(10.0)
+			print("work1 ended")
+		}
+		
+		func work2() {
+			print("work2 started")
+			NSThread.sleepForTimeInterval(15.0)
+			print("work2 ended")
+		}
+		
+		func work3() {
+			print("work3 started")
+			NSThread.sleepForTimeInterval(3.0)
+			print("work3 ended")
+		}
+		
+		func a1() {
+			dispatch_async(dispatch_get_global_queue(Int(QOS_CLASS_DEFAULT.rawValue), 0)) { () -> Void in
+				NSThread.sleepForTimeInterval(10.0)
+			}
+		}
+		
+		func a2() {
+			dispatch_async(dispatch_get_global_queue(Int(QOS_CLASS_DEFAULT.rawValue), 0)) { () -> Void in
+				NSThread.sleepForTimeInterval(10.0)
+			}
+		}
+		
+		dispatch_group_async(group, queue) { () -> Void in
+			a1()
+		}
+		
+		dispatch_group_async(group, queue) { () -> Void in
+			a2()
+		}
+		
+//		dispatch_group_async(group, queue) { () -> Void in
+//			work1()
+//		}
+//		
+//		dispatch_group_async(group, queue) { () -> Void in
+//			work2()
+//		}
+//		
+//		dispatch_group_async(group, queue) { () -> Void in
+//			work3()
+//		}
+		
+		dispatch_group_notify(group, queue) { () -> Void in
+			print("ok!")
+		}
+	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
